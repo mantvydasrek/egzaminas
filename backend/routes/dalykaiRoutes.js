22247c88e29a0614848490e6d902480620532744
mantvydasrek <mantvydas.rekus@gmail.com>
2026-06-25 10:13:26 +0300

    Refactor dalykaiRoutes to handle student subjects

--- backend/routes/dalykaiRoutes.js
+++ backend/routes/dalykaiRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/studentaiController');
+const controller = require('../controllers/dalykaiController');
 
-router.get('/', controller.gautiVisus);
-router.get('/:id', controller.gautiPagalId);
-router.post('/', controller.sukurti);
-router.put('/:id', controller.atnaujinti);
-router.delete('/:id', controller.istrinti);
+router.get('/studentas/:studentoId', controller.gautiStudentoDalykus);
+router.post('/studentas/:studentoId', controller.pridetiDalyka);
+router.put('/:id', controller.atnaujintiDalyka);
+router.delete('/:id', controller.istrintiDalyka);
 
 module.exports = router;
