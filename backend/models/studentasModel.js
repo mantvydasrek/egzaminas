6db1d1ea17e8d83a54be1f34a38b74955427d880
mantvydasrek <mantvydas.rekus@gmail.com>
2026-06-25 10:12:02 +0300

    Refactor studentasModel to include CRUD operations

--- backend/models/studentasModel.js
+++ backend/models/studentasModel.js
@@ -1,10 +1,63 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/dalykaiController');
+const db = require('../db/database');
 
-router.get('/studentas/:studentoId', controller.gautiStudentoDalykus);
-router.post('/studentas/:studentoId', controller.pridetiDalyka);
-router.put('/:id', controller.atnaujintiDalyka);
-router.delete('/:id', controller.istrintiDalyka);
+function gautiVisus(filtrai = {}) {
+  let sql = 'SELECT * FROM studentai WHERE 1=1';
+  const params = [];
 
-module.exports = router;
+  if (filtrai.course) {
+    sql += ' AND kursas = ?';
+    params.push(filtrai.course);
+  }
+  if (filtrai.id) {
+    sql += ' AND id = ?';
+    params.push(filtrai.id);
+  }
+  if (filtrai.vardas) {
+    sql += ' AND LOWER(vardas) LIKE ?';
+    params.push('%' + filtrai.vardas.toLowerCase() + '%');
+  }
+  if (filtrai.pavarde) {
+    sql += ' AND LOWER(pavarde) LIKE ?';
+    params.push('%' + filtrai.pavarde.toLowerCase() + '%');
+  }
+
+  sql += ' ORDER BY id DESC';
+  return db.prepare(sql).all(...params);
+}
+
+function gautiPagalId(id) {
+  return db.prepare('SELECT * FROM studentai WHERE id = ?').get(id);
+}
+
+function sukurti(vardas, pavarde, kursas) {
+  const rezultatas = db.prepare(
+    'INSERT INTO studentai (vardas, pavarde, kursas) VALUES (?, ?, ?)'
+  ).run(vardas, pavarde, kursas);
+  return gautiPagalId(rezultatas.lastInsertRowid);
+}
+
+function atnaujinti(id, vardas, pavarde, kursas) {
+  const esamas = gautiPagalId(id);
+  if (!esamas) return null;
+
+  db.prepare(
+    'UPDATE studentai SET vardas = ?, pavarde = ?, kursas = ? WHERE id = ?'
+  ).run(vardas, pavarde, kursas, id);
+
+  return gautiPagalId(id);
+}
+
+function istrinti(id) {
+  const esamas = gautiPagalId(id);
+  if (!esamas) return false;
+  db.prepare('DELETE FROM studentai WHERE id = ?').run(id);
+  return true;
+}
+
+module.exports = {
+  gautiVisus,
+  gautiPagalId,
+  sukurti,
+  atnaujinti,
+  istrinti
+};
