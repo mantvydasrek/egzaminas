6ce841a94b535a11449f7060ab6b14473c6794b3
mantvydasrek <mantvydas.rekus@gmail.com>
2026-06-25 10:11:29 +0300

    Update dalykasModel.js

--- backend/models/dalykasModel.js
+++ backend/models/dalykasModel.js
@@ -1,48 +1,29 @@
 const db = require('../db/database');
 
-function gautiVisus(filtrai = {}) {
-  let sql = 'SELECT * FROM studentai WHERE 1=1';
-  const params = [];
-
-  if (filtrai.course) {
-    sql += ' AND kursas = ?';
-    params.push(filtrai.course);
-  }
-  if (filtrai.id) {
-    sql += ' AND id = ?';
-    params.push(filtrai.id);
-  }
-  if (filtrai.vardas) {
-    sql += ' AND LOWER(vardas) LIKE ?';
-    params.push('%' + filtrai.vardas.toLowerCase() + '%');
-  }
-  if (filtrai.pavarde) {
-    sql += ' AND LOWER(pavarde) LIKE ?';
-    params.push('%' + filtrai.pavarde.toLowerCase() + '%');
-  }
-
-  sql += ' ORDER BY id DESC';
-  return db.prepare(sql).all(...params);
+function gautiPagalStudenta(studentoId) {
+  return db.prepare(
+    'SELECT * FROM dalykai WHERE studento_id = ? ORDER BY id'
+  ).all(studentoId);
 }
 
 function gautiPagalId(id) {
-  return db.prepare('SELECT * FROM studentai WHERE id = ?').get(id);
+  return db.prepare('SELECT * FROM dalykai WHERE id = ?').get(id);
 }
 
-function sukurti(vardas, pavarde, kursas) {
+function sukurti(studentoId, pavadinimas, kreditai) {
   const rezultatas = db.prepare(
-    'INSERT INTO studentai (vardas, pavarde, kursas) VALUES (?, ?, ?)'
-  ).run(vardas, pavarde, kursas);
+    'INSERT INTO dalykai (studento_id, pavadinimas, kreditai) VALUES (?, ?, ?)'
+  ).run(studentoId, pavadinimas, kreditai);
   return gautiPagalId(rezultatas.lastInsertRowid);
 }
 
-function atnaujinti(id, vardas, pavarde, kursas) {
+function atnaujinti(id, pavadinimas, kreditai) {
   const esamas = gautiPagalId(id);
   if (!esamas) return null;
 
   db.prepare(
-    'UPDATE studentai SET vardas = ?, pavarde = ?, kursas = ? WHERE id = ?'
-  ).run(vardas, pavarde, kursas, id);
+    'UPDATE dalykai SET pavadinimas = ?, kreditai = ? WHERE id = ?'
+  ).run(pavadinimas, kreditai, id);
 
   return gautiPagalId(id);
 }
@@ -50,12 +31,12 @@ function atnaujinti(id, vardas, pavarde, kursas) {
 function istrinti(id) {
   const esamas = gautiPagalId(id);
   if (!esamas) return false;
-  db.prepare('DELETE FROM studentai WHERE id = ?').run(id);
+  db.prepare('DELETE FROM dalykai WHERE id = ?').run(id);
   return true;
 }
 
 module.exports = {
-  gautiVisus,
+  gautiPagalStudenta,
   gautiPagalId,
   sukurti,
   atnaujinti,
